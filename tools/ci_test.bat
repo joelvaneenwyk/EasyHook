98e6840883f7d008e8ec2ccb98599ab4f06860af
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2020-03-06 09:53:17 -0800

    Copy AppVeyor adapter to Extensions folder

--- tools/ci_test.bat
+++ tools/ci_test.bat
@@ -21,10 +21,19 @@ nuget install -OutputDirectory %~dp0..\Packages Appveyor.TestLogger -Version 2.0
 nuget install -OutputDirectory %~dp0..\Packages Microsoft.TestPlatform -Version 16.5.0
 
 set ADAPTER_PATH=%~dp0..\Packages\Appveyor.TestLogger.2.0.0\build\_common
-dir %ADAPTER_PATH%
+set TEST_PLATFORM_ROOT=%~dp0..\Packages\Microsoft.TestPlatform.16.5.0\tools\net451\Common7\IDE\Extensions\TestPlatform
+
+set VSTEST=%TEST_PLATFORM_ROOT%\vstest.console.exe
+set VSTEST_EXTENSIONS=%TEST_PLATFORM_ROOT%\Extensions
+
+set APPVEYOR_TEST_ADAPTER_DLL=Microsoft.VisualStudio.TestPlatform.Extension.Appveyor.TestAdapter.dll
+set APPVEYOR_TEST_LOGGER_DLL=Microsoft.VisualStudio.TestPlatform.Extension.Appveyor.TestLogger.dll
+
+copy %ADAPTER_PATH%\%APPVEYOR_TEST_ADAPTER_DLL% %VSTEST_EXTENSIONS%\%APPVEYOR_TEST_ADAPTER_DLL%
+copy %ADAPTER_PATH%\%APPVEYOR_TEST_LOGGER_DLL% %VSTEST_EXTENSIONS%\%APPVEYOR_TEST_LOGGER_DLL%
 
 set VSTEST=%~dp0..\Packages\Microsoft.TestPlatform.16.5.0\tools\net451\Common7\IDE\Extensions\TestPlatform\vstest.console.exe
-set VSTEST_ARGS=/TestAdapterPath:%ADAPTER_PATH% /Logger:Appveyor /Parallel /Platform:%TEST_PLATFORM% "%APPVEYOR_BUILD_FOLDER%\Build\%CONFIGURATION%\%TEST_PLATFORM%\EasyHook.Tests.dll"
+set VSTEST_ARGS=/TestAdapterPath:%ADAPTER_PATH% /TestAdapterPath:%VSTEST_EXTENSIONS% /Logger:Appveyor /Parallel /Platform:%TEST_PLATFORM% "%APPVEYOR_BUILD_FOLDER%\Build\%CONFIGURATION%\%TEST_PLATFORM%\EasyHook.Tests.dll"
 
 echo.
 echo ================================================
