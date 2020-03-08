817e53954e3dd1c1a2b44f260d493f2542b37c6c
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2020-03-07 17:50:04 -0800

    Unify build script functionality

--- tools/ci_install.ps1
+++ tools/ci_install.ps1
@@ -1,5 +1,35 @@
 Write-Host "Running PowerShell install script."
 
+Function Add-PathVariable {
+    param (
+        [string]$addPath
+    )
+    if (Test-Path $addPath){
+        $regexAddPath = [regex]::Escape($addPath)
+        $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch 
+"^$regexAddPath\\?"}
+        $env:Path = ($arrPath + $addPath) -join ';'
+    } else {
+        Throw "'$addPath' is not a valid path."
+    }
+}
+
+$dir = [string](Get-Location)
+
+New-Item -ItemType Directory -Force -Path $PSScriptRoot\..\Bin\
+
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$targetNugetExe = "$PSScriptRoot\..\Bin\nuget.exe"
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+nuget restore "$PSScriptRoot\..\EasyHook.sln"
+
+nuget install -OutputDirectory $PSScriptRoot\..\Packages Microsoft.TestPlatform -Version 16.5.0
+nuget install -OutputDirectory $PSScriptRoot\..\Packages vswhere -Version 2.8.4
+nuget install -OutputDirectory $PSScriptRoot\..\Packages MSBuildTasks -Version 1.5.0.196
+nuget install -OutputDirectory $PSScriptRoot\..\Packages Appveyor.TestLogger -Version 2.0.0
+
 $msiPath = "$($env:USERPROFILE)\CoApp.Tools.Powershell.msi"
 
 (New-Object Net.WebClient).DownloadFile('https://easyhook.github.io/downloads/CoApp.Tools.Powershell.msi', $msiPath)
@@ -7,9 +37,7 @@ $msiPath = "$($env:USERPROFILE)\CoApp.Tools.Powershell.msi"
 cmd /c start /wait msiexec /i "$msiPath" /quiet
 
 # Update environment path
-
 $env:PSModulePath = $env:PSModulePath + ';C:\Program Files (x86)\Outercurve Foundation\Modules'
 
 # Import CoApp module (for packaging native NuGet)
-
 Import-Module CoApp
--- tools/ci_test.bat
+++ tools/ci_test.bat
@@ -1,45 +1,6 @@
 @echo off
 
-if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0..\
-if "%PLATFORM%" == "" set PLATFORM=Win32
-if "%CONFIGURATION%" == "" set CONFIGURATION=netfx3.5-Debug
-
-set TEST_PLATFORM=x64
-if "%PLATFORM%" == "Win32" set TEST_PLATFORM=x86
-
-set PATH=%~dp0..\Bin;%PATH%
-
-echo.
-echo ================================================
-echo.
-set
-echo.
-echo ================================================
-echo.
-
-nuget install -OutputDirectory %~dp0..\Packages Appveyor.TestLogger -Version 2.0.0
-nuget install -OutputDirectory %~dp0..\Packages Microsoft.TestPlatform -Version 16.5.0
-
-set ADAPTER_PATH=%~dp0..\Packages\Appveyor.TestLogger.2.0.0\build\_common
-set TEST_PLATFORM_ROOT=%~dp0..\Packages\Microsoft.TestPlatform.16.5.0\tools\net451\Common7\IDE\Extensions\TestPlatform
-
-set VSTEST=%TEST_PLATFORM_ROOT%\vstest.console.exe
-set VSTEST_EXTENSIONS=%TEST_PLATFORM_ROOT%\Extensions
-
-set APPVEYOR_TEST_ADAPTER_DLL=Microsoft.VisualStudio.TestPlatform.Extension.Appveyor.TestAdapter.dll
-set APPVEYOR_TEST_LOGGER_DLL=Microsoft.VisualStudio.TestPlatform.Extension.Appveyor.TestLogger.dll
-
-copy %ADAPTER_PATH%\%APPVEYOR_TEST_ADAPTER_DLL% %VSTEST_EXTENSIONS%\%APPVEYOR_TEST_ADAPTER_DLL%
-copy %ADAPTER_PATH%\%APPVEYOR_TEST_LOGGER_DLL% %VSTEST_EXTENSIONS%\%APPVEYOR_TEST_LOGGER_DLL%
-
-set VisualStudioTestExplorerExtensions=
-set VSTEST=%TEST_PLATFORM_ROOT%\vstest.console.exe
-set VSTEST_ARGS=/TestAdapterPath:%ADAPTER_PATH% /TestAdapterPath:%VSTEST_EXTENSIONS% /Logger:Appveyor /Parallel /Platform:%TEST_PLATFORM% "%APPVEYOR_BUILD_FOLDER%\Build\%CONFIGURATION%\%TEST_PLATFORM%\EasyHook.Tests.dll"
-
-echo.
-echo ================================================
-echo.
-
+call %~dp0\setup.bat
 
 :: This calls the newest version of vstest.console.exe but for some reason even with correct adapater
 :: paths it is not able to find Appveyor
