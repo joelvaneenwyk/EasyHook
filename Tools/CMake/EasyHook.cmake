7edcf03b0b4d574b4574918a5a1ad27785589828
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-11-25 23:15:04 -0800

    Updated CMake configs

--- Tools/CMake/EasyHook.cmake
+++ Tools/CMake/EasyHook.cmake
@@ -1,61 +1,79 @@
-# Create assembly info with current version.
-# CONFIGURE_FILE("${CMAKE_SOURCE_DIR}/src/AssemblyInfo.cs.template" "${CMAKE_BINARY_DIR}/Config/${PROJECT_NAME}/AssemblyInfo.cs")
+# EasyHook (File: Tools/CMake/EasyHook.cmake)
+#
+# Copyright (c) 2009 Christoph Husse & Copyright (c) 2015 Justin Stenning
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Please visit https://easyhook.github.io for more information
+# about the project and latest updates.
 
-SET(TARGET_NAME EasyHook)
-# Add shared library project.
-ADD_LIBRARY(${TARGET_NAME} SHARED
-    "${CMAKE_SOURCE_DIR}/EasyHook/COMClassInfo.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/Config.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/Debugging.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/DllImport.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/GACWrap.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/HelperServiceInterface.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/InjectionLoader.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/LocalHook.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/NativeMethods.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/RemoteAsyncStreamReader.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/RemoteHook.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/RemoteHookProcess.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/ServiceMgmt.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/WOW64Bypass.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/Domain/DomainIdentifier.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/-DummyCore.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/ChannelProperties.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/ConnectionManager.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/DomainConnectionEndPoint.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannel.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelEndPointObject.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelReadyEventHandler.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelState.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/EndPointConfigurationData.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/EndPointObject.cs"
-    "${CMAKE_SOURCE_DIR}/EasyHook/IPC/SimplexChannel.cs"
-)
+set(TARGET_NAME EasyHook)
+
+add_library(${TARGET_NAME} SHARED
+		"${CMAKE_SOURCE_DIR}/EasyHook/COMClassInfo.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/Config.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/Debugging.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/DllImport.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/GACWrap.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/HelperServiceInterface.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/InjectionLoader.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/LocalHook.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/NativeMethods.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/RemoteAsyncStreamReader.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/RemoteHook.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/RemoteHookProcess.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/ServiceMgmt.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/WOW64Bypass.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/Domain/DomainIdentifier.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/-DummyCore.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/ChannelProperties.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/ConnectionManager.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/DomainConnectionEndPoint.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannel.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelEndPointObject.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelReadyEventHandler.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/DuplexChannelState.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/EndPointConfigurationData.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/EndPointObject.cs"
+		"${CMAKE_SOURCE_DIR}/EasyHook/IPC/SimplexChannel.cs"
+		)
 
 # Set designer and XAML properties.
-# CSHARP_SET_DESIGNER_CS_PROPERTIES(
-#     "${CMAKE_SOURCE_DIR}/EasyHook/Properties/AssemblyInfo.cs"
-# )
-
-# Define dependencies.
-# TARGET_LINK_LIBRARIES(${TARGET_NAME}
-#     PUBLIC CommonLib
-# )
+csharp_set_designer_cs_properties(
+		"${CMAKE_SOURCE_DIR}/EasyHook/Properties/AssemblyInfo.cs"
+)
 
 # Set CLR assembly properties.
-SET_TARGET_PROPERTIES(${TARGET_NAME} PROPERTIES
-    VS_DOTNET_REFERENCES "System;System.Data;System.Runtime.Remoting;System.Xml"
-    VS_GLOBAL_ROOTNAMESPACE ${TARGET_NAME}
-)
+set_target_properties(${TARGET_NAME} PROPERTIES
+		VS_DOTNET_REFERENCES "System;System.Data;System.Runtime.Remoting;System.Xml"
+		VS_GLOBAL_ROOTNAMESPACE ${TARGET_NAME}
+		)
+
 target_compile_options(${TARGET_NAME} PUBLIC "/unsafe")
 
 # Setup installer.
-INSTALL(TARGETS ${TARGET_NAME} EXPORT ${TARGET_NAME}Config
-    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBRARY_DIR}
-    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBRARY_DIR}
-    RUNTIME DESTINATION ${CMAKE_INSTALL_BINARY_DIR}
-    INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDE_DIR}
-)
+install(TARGETS ${TARGET_NAME} EXPORT ${TARGET_NAME}Config
+		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBRARY_DIR}
+		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBRARY_DIR}
+		RUNTIME DESTINATION ${CMAKE_INSTALL_BINARY_DIR}
+		INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDE_DIR}
+		)
 
 # Export config.
 # INSTALL(EXPORT ${TARGET_NAME} DESTINATION ${CMAKE_INSTALL_EXPORT_DIR})
--- Tools/CMake/EasyHookDll.cmake
+++ Tools/CMake/EasyHookDll.cmake
@@ -1,36 +1,58 @@
+# EasyHook (File: Tools/CMake/EasyHookDll.cmake)
+#
+# Copyright (c) 2009 Christoph Husse & Copyright (c) 2015 Justin Stenning
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Please visit https://easyhook.github.io for more information
+# about the project and latest updates.
 
-SET(TARGET_NAME EasyHookDll)
+set(TARGET_NAME EasyHookDll)
+set(TARGET_ARCH 64)
+
+# "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB.H"
+# "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB_x64.LIB"
+# "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB_x86.LIB"
 
 add_library(${TARGET_NAME}
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB.H"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB_x64.LIB"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/AUX_ULIB_x86.LIB"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/dllmain.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHook32.def"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHook64.def"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll.vcxproj"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll.vcxproj.filters"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll.vcxproj.user"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll_32.rc"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll_64.rc"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/gacutil.cpp"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/LocalHook"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/ntstatus.h"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/resource.h"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/Rtl"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/stdafx.h"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/LocalHook/acl.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/LocalHook/debug.cpp"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/driver.cpp"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/entry.cpp"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/service.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/stealth.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/thread.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/Rtl/file.c"
-    "${CMAKE_SOURCE_DIR}/EasyHookDll/Rtl/memory.c")
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/dllmain.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHook${TARGET_ARCH}.def"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHookDll_${TARGET_ARCH}.rc"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/gacutil.cpp"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/ntstatus.h"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/resource.h"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/stdafx.h"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/LocalHook/acl.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/LocalHook/debug.cpp"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/driver.cpp"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/entry.cpp"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/service.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/stealth.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/RemoteHook/thread.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/Rtl/file.c"
+		"${CMAKE_SOURCE_DIR}/EasyHookDll/Rtl/memory.c")
 
 target_include_directories(${TARGET_NAME} AFTER
 		PUBLIC "${CMAKE_SOURCE_DIR}/EasyHookDll"
 		PUBLIC "${CMAKE_SOURCE_DIR}/Public"
 		PUBLIC "${CMAKE_SOURCE_DIR}/DriverShared")
+
+set_source_files_properties("${CMAKE_SOURCE_DIR}/EasyHookDll/EasyHook${TARGET_ARCH}.def" PROPERTIES
+		HEADER_FILE_ONLY TRUE)
