08333eaf5936030bd8dd44af5460672aab28b877
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-23 13:59:24 -0700

    more fixes

--- EasyHook/DllImport.cs
+++ EasyHook/DllImport.cs
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,13 +27,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using JetBrains.Annotations;
 using Microsoft.Win32.SafeHandles;
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace EasyHook
 {
-    [PublicAPI]
     static class NativeAPI_EasyHook
     {
         static bool _initialised = false;
@@ -91,7 +89,7 @@ namespace EasyHook
         static object _mutex = new object();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <exception cref="DllNotFoundException" />
         static void Initialise()
