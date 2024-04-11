9262a3c240fe6dd5112ca6da1591696fa9b83ba6
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-11 11:45:58 -0700

    script updates

--- build.bat
+++ build.bat
@@ -30,13 +30,4 @@ endlocal & (
     )
 
     call msbuild %*
-    :: echo ##[cmd] call "%~dp0Tools\setup.bat" %*
-    :: call "%~dp0Tools\setup.bat" %*
-    :: if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-    :: if not exist "%POWERSHELL%" (
-    ::     echo PowerShell not found. Please install PowerShell 3.0 or later.
-    ::     exit /b 88
-    :: )
-    :: echo ##[cmd] "%POWERSHELL%" %POWERSHELL_CONSOLE% "& '%~dp0Tools\build.ps1' %*;"
-    :: "%POWERSHELL%" %POWERSHELL_CONSOLE% "& '%~dp0Tools\build.ps1' %*;"
-exit /b 0
+goto:eof
